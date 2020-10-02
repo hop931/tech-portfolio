8945d1a3d38c7a0c7ad66b2a43b16015c33bb6c9
hop931 <72216286+hop931@users.noreply.github.com>
2020-10-02 12:25:00 -0700

    I started my tech portfolio

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Haley Prengaman', // Title for your website.
+    tagline: 'Asprising Project Manager working with rocket ships',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://hop931.github.io', // Your website URL: https://github.com/hop931
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'hop931',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'haley-prengaman-4b38a7140/',
         twitter: '',
-        github: '',
+        github: 'hop931',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
